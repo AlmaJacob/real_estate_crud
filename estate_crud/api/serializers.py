885c1de37d321e0a7517394fa2103793ef4e72a6
Alma <almajacob7@gmail.com>
2025-08-09 11:38:51 +0530

    cart set up completed

--- estate_crud/api/serializers.py
+++ estate_crud/api/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from .models import estate, Cart ,User
 
-
+#CRUD of estate (detail page adding)
 class EstateDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = estate
         fields = '__all__'
-
+#Listing the estate products
 class EstateListSerializer(serializers.ModelSerializer):
     class Meta:
         model = estate
@@ -18,7 +18,7 @@ class CartSerializer(serializers.ModelSerializer):
         model = Cart
         fields = ['id', 'estate']
 
-
+#registation of user
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
